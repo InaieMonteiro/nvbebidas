12fc767cda1f857c122a184f476768d548c6443c
thais <thaisinaiemonteiro@gmail.com>
2023-10-10 09:34:07 -0300

    edit

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -1,76 +1,62 @@
-import './index.scss';
+import face from '../../assets/images/face.png'
+import linkedin from '../../assets/images/linkedin.png'
+import google from '../../../src/assets/images/google.png'
+import mobile from '../../assets/images/celular_png.png'
+import './index.scss'
 
-import logo from '../../assets/images/logo.png';
-import facebook from '../../assets/images/facebook.png';
-import google from '../../assets/images/google.png';
-
-
-export default function () {
-
+function Cadastro() {
     return (
-        <div className="pag-inicial123">
 
-            <div className='cabecalho'>
-                <img src={logo}></img>
-                <h1>THE RUNNERS BURGUERS</h1>
+        <div className='pag1'>
 
-            </div>
-
-            <div className='corpo'>
-                <h2>CRIAR CONTA</h2>
-            </div>
-
-            <div className='info'>
-                <input className='senha' placeholder='EMAIL:' type='email'></input>
-            </div>
-
-            <div className='info2'>
-                <input className='senha' placeholder='SENHA:' type='password'></input>
-            </div>
-
-            <div className='info3'>
-                <input className='nome' placeholder='NOME:' type='text'></input>
-            </div>
-
-            <div className='info4'>
-                <input className='sobrenome' placeholder='SOBRENOME' type='text'></input>
-            </div>
-
-
-
-
-
-        <div className='inputs'>
-
-
-                <div className='info7'> 
-                        <h1>PROSSEGUIR</h1>
-                </div>
-
-
-
-                <div className='info8'> 
-                    <img src={facebook}></img>
-                     <h1>FACEBOOK</h1>
-                  </div>
-
-
-                  <div className='info9'> 
-                  <img src={google}></img>
-                  <h1>GOOGLE</h1>
-                    </div>
-
-
-
+         <main>
+        <h1>Criar Conta</h1>
+        <div className="social-media">
+            <a href="#">
+                <img src={face}></img>
+            </a>
+            <a href="#">
+            <img src={google}></img>
+            </a>
+            <a href="#">
+            <img src={linkedin}></img>
+            </a>
         </div>
 
+        <div className="alternative">
+            <span>OU</span>
+        </div>
+
+        <form action="">
+            <label for="name">
+                <span>Nome</span>
+                <input type="text" id="name" name="name"/>
+            </label>
+
+            <label for="email">
+                <span>E-mail</span>
+                <input type="email" id="email" name="email"/>
+            </label>
+
+            <label for="password">
+                <span>Senha</span>
+                <input type="password" id="password" name="password"/>
+            </label>
+
+            <input type="submit" value="Prosseguir" id="button"/>
+
+            </form>
+    </main>
+    <section class="images">
+        <img src={mobile} alt="Mobile"></img>
+
+    </section>
 
     </div>
 
 
 
-
-
     );
-
 }
+
+export default Cadastro;
