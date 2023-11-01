98c5196ed67bb1bff417b8f7e6d8681edc12778a
thais <thaisinaiemonteiro@gmail.com>
2023-11-01 11:50:13 -0300

    italac

--- src/pages/introducao/index.js
+++ src/pages/introducao/index.js
@@ -1,3 +1,4 @@
+
 import './index.scss';
 import burguer from '../../assets/images/cheddarcardapio.png';
 import fritas from '../../assets/images/batcardapio.png';
@@ -5,67 +6,70 @@ import bebidas from '../../assets/images/bebcardapio.png';
 
 
 export default function introducao() {
-    return(
+    return (
         <div className='caixa1'>
-          <div className='caixa2'>
-            <h1>Cardápio</h1>
+            <div className='caixa2'>
+                <h1>Cardápio</h1>
             </div>
             <h1 className='t1'>Conheça Nossos Lanches</h1>
+
             <div className='c1'>
 
-            <div className='h2'>
-                <img src={burguer} /> 
-                <h2>Lanches</h2></div>
+                <div className='h2'>
+                    <img src={burguer} />
+                    <h2>Lanches</h2></div>
+
 
                 <div className='para'>
-                <p> Uma explosão de sabores em cada mordida para<br></br> 
-                acompanhar a sua correria. Sabor que te faz<br>
-                </br> acelerar!</p>
+                    <div className='corzinhadoita'></div>
+                    <p> Uma explosão de sabores em cada mordida para<br></br>
+                        acompanhar a sua correria. Sabor que te faz<br>
+                        </br> acelerar!</p>
+                    <div className='comeco'>
+                        <button href='http://localhost:3000/cardapio'> Ver opções </button>
+                    </div>    
+                    
                 </div>
-
-                 <div className='comeco'>
-                <button> Ver opções </button>
-                </div> 
-
-            </div> 
+                <div id='teste'></div>
+            </div>
 
             <h1 className='t2'>Conheça Nossas Batatas</h1>
             <div className='c2'>
-                
-            <div className='h3'>
-                <img src={fritas} />
-                <h2>Fritas</h2>
+
+                <div className='h3'>
+                    <img src={fritas} />
+                    <h2>Fritas</h2>
                 </div>
 
                 <div className='gra'>
-                <p> Uma explosão de sabor e crocância para fazer o seu<br></br> 
-                paladar correr de alegria. Deliciosamente crocantes e <br>
-                </br> irresistíveis!"</p>
+                    <p> Uma explosão de sabor e crocância para fazer o seu<br></br>
+                        paladar correr de alegria. Deliciosamente crocantes e <br>
+                        </br> irresistíveis!"</p>
                 </div>
 
                 <div className='meio'>
-                <button >Ver opções</button>
+                    <button onClick='http://localhost:3000/cardapio' >Ver opções</button>
                 </div>
             </div>
-            
+
             <h1 className='t3'>Se hidrate</h1>
             <div className='c3'>
                 <div className='h4'>
-                <img src={bebidas} />
-                <h2>Bebidas</h2>
+                    <img src={bebidas} />
+                    <h2>Bebidas</h2>
                 </div>
 
                 <div className='fo'>
-                <p>  A  corrida mais saborosa para saciar a sua sede.<br></br>
-                 Acompanhe seu sprint de sabores com nossas opções<br>
-                 </br> refrescantes e hidrate seu paladar.</p>
-                 </div>
-
-                <div className='final'>
-                <button>Ver opções</button>
+                    <p>  A  corrida mais saborosa para saciar a sua sede.<br></br>
+                        Acompanhe seu sprint de sabores com nossas opções<br>
+                        </br> refrescantes e hidrate seu paladar.</p>
                 </div>
 
-            </div>          
+                <div className='final'>
+                    <button href='http://localhost:3000/cardapio'>Ver opções</button>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/pages/primeira/index.js
+++ src/pages/primeira/index.js
@@ -14,7 +14,6 @@ export default function () {
         navigate('/cadastroAdm')
     }
 
-
     return (
 
         <div className='corzinha'>
