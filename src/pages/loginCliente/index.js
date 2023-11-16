d0e906dbafa25c6ab9d1f89bcca5983581f9d3db
thais <thaisinaiemonteiro@gmail.com>
2023-11-16 11:55:06 -0300

    gg

--- src/pages/loginCliente/index.js
+++ src/pages/loginCliente/index.js
@@ -1,10 +1,16 @@
+
+import { useState } from 'react';
 import './index.scss';
 import celular from '../cadastro/img/celular.png'
 import face from '../cadastro/img/facebook.png'
 import google from '../cadastro/img/google.png'
 
+const [email, setEmail] = useState('');
+const [senha, setSenha] = useState ('');
 
+function entrarClick() {
 
+}
 
 function Cadastro() {
     return (
@@ -34,18 +40,19 @@ function Cadastro() {
 
             <label for="email">
                 <span>E-mail</span>
-                <input type="email" id="email" name="email"/>
+                <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)}/>
             </label>
 
             <label for="password">
                 <span>Senha</span>
-                <input type="password" id="password" name="password"/>
+                <input type="password" id="password" name="password" value={senha} onChange={e => setSenha(e.target.value)}/>
             </label>
 
          
 
 
-            <input type="submit" value="Prosseguir" id="button"/>
+            <input type="submit" value="Prosseguir"id="button"/>
+            <button className='button' onClick={entrarClick} Entrar></button>
 
             </form>
           
