7922d0ad259f3be5c4159a1608d48e3869258638
Inaiê Monteiro <141843021+InaieMonteiro@users.noreply.github.com>
2023-11-23 09:29:35 -0300

    s

--- src/pages/loginCliente/index.js
+++ src/pages/loginCliente/index.js
@@ -1,73 +1,73 @@
 
-import { useState } from 'react';
-import './index.scss';
-import celular from '../cadastro/img/celular.png'
-import face from '../cadastro/img/facebook.png'
-import google from '../cadastro/img/google.png'
+// import { useState } from 'react';
+// import './index.scss';
+// import celular from '../cadastro/img/celular.png'
+// import face from '../cadastro/img/facebook.png'
+// import google from '../cadastro/img/google.png'
 
-const [email, setEmail] = useState('');
-const [senha, setSenha] = useState ('');
+// const [email, setEmail] = useState('');
+// const [senha, setSenha] = useState ('');
 
-function entrarClick() {
+// function entrarClick() {
 
-}
+// }
 
-function Cadastro() {
-    return (
+// function Cadastro() {
+//     return (
 
-        <div className='mae111'>
+//         <div className='mae111'>
 
-         <main>
-        <h1>Faça Login</h1>
-        <div className="social-media">
-            <a href="#">
-             <img src={google} />
-            </a>
-            <a href="">
-            <img src={face} />
-            </a>
-            <a href="#">
+//          <main>
+//         <h1>Faça Login</h1>
+//         <div className="social-media">
+//             <a href="#">
+//              <img src={google} />
+//             </a>
+//             <a href="">
+//             <img src={face} />
+//             </a>
+//             <a href="#">
           
-            </a>
-        </div>
+//             </a>
+//         </div>
 
-        <div className="alternative">
-            <span>OU</span>
-        </div>
+//         <div className="alternative">
+//             <span>OU</span>
+//         </div>
 
-        <form action="">
+//         <form action="">
           
 
-            <label for="email">
-                <span>E-mail</span>
-                <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)}/>
-            </label>
+//             <label for="email">
+//                 <span>E-mail</span>
+//                 <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)}/>
+//             </label>
 
-            <label for="password">
-                <span>Senha</span>
-                <input type="password" id="password" name="password" value={senha} onChange={e => setSenha(e.target.value)}/>
-            </label>
+//             <label for="password">
+//                 <span>Senha</span>
+//                 <input type="password" id="password" name="password" value={senha} onChange={e => setSenha(e.target.value)}/>
+//             </label>
 
          
 
 
-            <input type="submit" value="Prosseguir"id="button"/>
-            <button className='button' onClick={entrarClick} Entrar></button>
+//             <input type="submit" value="Prosseguir"id="button"/>
+//             <button className='button' onClick={entrarClick} Entrar></button>
 
-            </form>
+//             </form>
           
 
-    </main>
-    <section class="images">
-        <img src={celular} />
+//     </main>
+//     <section class="images">
+//         <img src={celular} />
 
-    </section>
+//     </section>
 
-    </div>
+//     </div>
 
 
 
-    );
-}
+//     );
+// }
 
-export default Cadastro;
+// export default Cadastro;
